4e0b96ab6b9dfd3ff3f8535fe483aad17f949cb1
fausio luis <luisfausio@gmail.com>
2023-12-05 21:49:21 +0200

    Add project files.

---
+++ logic.systems.school.managment/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace logic.systems.school.managment.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
