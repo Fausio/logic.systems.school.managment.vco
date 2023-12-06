e5dfbb199a07f77409230d3a1b33cf97081bef6b
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-12-06 10:50:20 +0200

    up migration and db

---
+++ logic.systems.school.managment/Helper/Appsettings.cs
@@ -0,0 +1,7 @@
+﻿namespace logic.systems.school.managment.Helper
+{
+    public static class Appsettings
+    {
+        public const string DefaultConnection = "Server=tcp:fserver01.database.windows.net,1433;Initial Catalog=logic-management-system.teste;Persist Security Info=False;User ID=sys.admin;Password=FHI0l0ga**;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
--- logic.systems.school.managment/Models/Tuition.cs
+++ logic.systems.school.managment/Models/Tuition.cs
@@ -6,7 +6,7 @@ namespace logic.systems.school.managment.Models
     public class Tuition : Common
     {
         public SchoolLevel AssociatedLevel { get; set; }
-        public SchoolLevel AssociatedLevelId { get; set; }
+        public int AssociatedLevelId { get; set; }
         public decimal MonthlyFee { get; set; }
     }
 }
