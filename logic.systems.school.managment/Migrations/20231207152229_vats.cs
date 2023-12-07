bc7d8f20b2f1c35c9b7c4dd6576af11b6bc56e40
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-12-07 17:24:27 +0200

    up

---
+++ logic.systems.school.managment/Migrations/20231207152229_vats.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace logic.systems.school.managment.Migrations
+{
+    public partial class vats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MonthlyFee",
+                table: "Tuition",
+                newName: "VatOfMonthlyFee");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MonthlyFeeWithVat",
+                table: "Tuition",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MonthlyFeeWithoutVat",
+                table: "Tuition",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TuitionDate",
+                table: "Tuition",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MonthlyFeeWithVat",
+                table: "Tuition");
+
+            migrationBuilder.DropColumn(
+                name: "MonthlyFeeWithoutVat",
+                table: "Tuition");
+
+            migrationBuilder.DropColumn(
+                name: "TuitionDate",
+                table: "Tuition");
+
+           
+            migrationBuilder.RenameColumn(
+                name: "VatOfMonthlyFee",
+                table: "Tuition",
+                newName: "MonthlyFee");
+        }
+    }
+}
--- logic.systems.school.managment/Models/Tuition.cs
+++ logic.systems.school.managment/Models/Tuition.cs
@@ -5,8 +5,12 @@ namespace logic.systems.school.managment.Models
     [Table("Tuition")]
     public class Tuition : Common
     {
+
         public SchoolLevel AssociatedLevel { get; set; }
         public int AssociatedLevelId { get; set; }
-        public decimal MonthlyFee { get; set; }
+        public DateTime TuitionDate { get; set; }
+        public decimal MonthlyFeeWithoutVat { get; set; }
+        public decimal VatOfMonthlyFee { get; set; } // 5%
+        public decimal MonthlyFeeWithVat { get; set; }
     }
 }
