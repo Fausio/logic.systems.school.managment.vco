500abaf51514cb5137915198c534bcefa06cd9b7
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-12-11 15:16:58 +0200

    seed

--- logic.systems.school.managment/Program.cs
+++ logic.systems.school.managment/Program.cs
@@ -1,30 +1,40 @@
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using DocumentFormat.OpenXml.Spreadsheet;
 using logic.systems.school.managment.Data;
 using logic.systems.school.managment.Interface;
 using logic.systems.school.managment.Models;
 using logic.systems.school.managment.Seeds;
 using logic.systems.school.managment.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>     options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => {
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 4;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    })
+    .AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 
 builder.Services.AddScoped<IstudantService, StudantService>();
 builder.Services.AddScoped<IOrgUnit, OrgUnitService>();
 builder.Services.AddScoped<ISempleEntityService, SempleEntityService>();
-  builder.Services.AddScoped<ITuitionService, TuitionService>();
-  builder.Services.AddScoped<IDashBoard, DashboardService>();
-  builder.Services.AddScoped<Idocument, DocumentService>();
-                                                        
+builder.Services.AddScoped<ITuitionService, TuitionService>();
+builder.Services.AddScoped<IDashBoard, DashboardService>();
+builder.Services.AddScoped<Idocument, DocumentService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -52,13 +62,50 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
+
+
+
 using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider  .GetRequiredService<ApplicationDbContext>();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roles = new string[] { "Administrator".ToUpper(), "employee".ToUpper() };
+    foreach (string role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
+using (var scope = app.Services.CreateScope())
+{
+    var UserManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
+    var mail = "admin@Kalimany.com";
+    var pass = "admin1234";
+    if (await UserManager.FindByEmailAsync(mail) is null)
+    {
+        var user = new IdentityUser()
+        {
+            Email = mail,
+            NormalizedEmail = "admin@Kalimany.com",
+            UserName = "admin",
+            NormalizedUserName = "admin",
+            EmailConfirmed = true,
+            PhoneNumberConfirmed = true,
+        };
+
+        await UserManager.CreateAsync(user, pass);
+        await UserManager.AddToRoleAsync(user, "ADMINISTRATOR");
+    }
+}
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     // Here is the migration executed
     dbContext.Database.Migrate();
 }
+
 await SeedOrgUnit.Run();
 await SeedSimpleEntity.Run();
 
--- logic.systems.school.managment/Seeds/SeedUsers.cs
+++ logic.systems.school.managment/Seeds/SeedUsers.cs
@@ -1,29 +1,61 @@
-﻿using logic.systems.school.managment.Data;
+﻿using DocumentFormat.OpenXml.InkML;
+using logic.systems.school.managment.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace logic.systems.school.managment.Seeds
 {
     public static class SeedUsers
     {
-        public static async Task Run()
+        public async static Task Run(IServiceProvider serviceProvider)
         {
-           
+
+            var db = serviceProvider.GetService<ApplicationDbContext>();
+
+            string[] roles = new string[] { "Administrator".ToUpper(), "employee".ToUpper() };
+            foreach (string role in roles)
+            {
+                var roleStore = new RoleStore<IdentityRole>(db);
+
+                if (!await db.Roles.AnyAsync(r => r.Name == role))
+                {
+                    await roleStore.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            var user = new IdentityUser
+            {
+
+                Email = "admin@Kalimany.com",
+                NormalizedEmail = "admin@Kalimany.com",
+                UserName = "admin",
+                NormalizedUserName = "admin",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString("D")
+            };
+
+            if (!await db.Users.AnyAsync(u => u.UserName == user.UserName))
+            {
+                var password = new PasswordHasher<IdentityUser>();
+                var hashed = password.HashPassword(user, "admin1234");
+                user.PasswordHash = hashed;
+
+                var userStore = new UserStore<IdentityUser>(db);
+                var result = userStore.CreateAsync(user);
+            }
+
+            await AssignRoles(serviceProvider, user.Email, roles);
+            await db.SaveChangesAsync();
         }
 
-        private static async Task Seed(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        public async static Task<IdentityResult> AssignRoles(IServiceProvider services, string email, string[] roles)
         {
-            var db = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>());
-
-            // Criar funções (roles) se não existirem
-            if (!roleManager.RoleExistsAsync("Admin").Result)
-            {
-                var role = new IdentityRole
-                {
-                    Name = "Admin"
-                };
-                roleManager.CreateAsync(role).Wait();
-            }
+            UserManager<IdentityUser> _userManager = services.GetService<UserManager<IdentityUser>>();
+            IdentityUser user = await _userManager.FindByEmailAsync(email);
+            var result = await _userManager.AddToRolesAsync(user, roles);
+            return result;
         }
     }
 }
