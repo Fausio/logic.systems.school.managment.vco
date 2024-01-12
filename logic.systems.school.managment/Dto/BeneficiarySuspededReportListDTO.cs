733c94728fa85380de8ae7a3f723efbecebc4e0f
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2024-01-12 17:14:37 +0200

    ups

--- logic.systems.school.managment/Dto/BeneficiarySuspededReportListDTO.cs
+++ logic.systems.school.managment/Dto/BeneficiarySuspededReportListDTO.cs
@@ -5,10 +5,15 @@
         public DateTime EmissionDate { get; set; }
         public string StudendName { get; set; }
         public string StudentClassLevel { get; set; }
-        public string Month { get; set; }
-        public decimal TuituinValue { get; set; }
+
+        public string MonthTuituin { get; set; }
+        public decimal MonthTuituinValue { get; set; }
         public decimal PaymentTerm_first { get; set; }
         public decimal PaymentTerm_Secund { get; set; }
         public decimal TuituinPaimentStatus { get; set; }
+
+        public decimal MonthTuituinFee { get; set; }
+        public decimal MonthTuituinFeeStatus { get; set; }
+
     }
 }
