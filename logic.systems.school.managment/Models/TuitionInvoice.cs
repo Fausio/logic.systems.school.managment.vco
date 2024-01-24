9eb98f72d41fc993e4a089ad10c4e6ca5c46ce92
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2024-01-24 09:07:32 +0200

    up

--- logic.systems.school.managment/Models/TuitionInvoice.cs
+++ logic.systems.school.managment/Models/TuitionInvoice.cs
@@ -4,8 +4,7 @@ namespace logic.systems.school.managment.Models
 {
     [Table("TuitionInvoice")]
     public class TuitionInvoice : Invoice
-    { 
-
+    {
         public TuitionInvoice()
         {
             this.Type = "TuitionInvoice";
