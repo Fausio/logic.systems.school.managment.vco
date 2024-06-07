7bc19f306f64ef2623eeb7fe3f106ed516ad12e7
fausio luis <luisfausio@gmail.com>
2024-06-07 05:45:33 +0200

    user config

---
+++ logic.systems.school.managment/Dto/UpdateUserInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace logic.systems.school.managment.Dto
+{
+    public class UpdateUserInfoDTO
+    {
+        public string UserId { get; set; }
+        [Required(ErrorMessage = "O email é Obrigatório")]
+        [EmailAddress(ErrorMessage = "Deve adicionar um email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+ 
+    }
+}
