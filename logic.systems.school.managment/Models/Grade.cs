ff9f5c1f89263149a2e2e9bfc3e0369023b87ef3
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2024-07-01 13:20:14 +0200

    done

--- logic.systems.school.managment/Models/Grade.cs
+++ logic.systems.school.managment/Models/Grade.cs
@@ -7,11 +7,9 @@ namespace logic.systems.school.managment.Models
     public class Grade : Common
     {
         public const string GradeType_ACS = "ACS";
-        public const string GradeType_AP = "AP";
-
+        public const string GradeType_AP = "AP"; 
         public Assessment? Assessment { get; set; }
-        public int? AssessmentId { get; set; } 
-
+        public int? AssessmentId { get; set; }  
         public DateTime? Date { get; set; }
         public decimal? Value { get; set; }  
         public string Type { get; set; }
