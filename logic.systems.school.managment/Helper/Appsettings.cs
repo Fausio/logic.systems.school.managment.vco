ef74923643dd046b61d6594cecdf5c4c95b3fd32
fausio luis <fausioluis@live.com>
2025-01-28 00:22:11 +0200

    DefaultConnection

--- logic.systems.school.managment/Helper/Appsettings.cs
+++ logic.systems.school.managment/Helper/Appsettings.cs
@@ -2,7 +2,7 @@
 {
     public static class Appsettings
     {
-        public const string DefaultConnection = "Server=51.91.14.5;Database=logicsys_logic.systems.school.managment.panda;Trusted_Connection=false;MultipleActiveResultSets=true;Persist Security Info=true;User Id=AmeconAssociation;Password=5hbXx5%02;";
+        public const string DefaultConnection = @"Server=wgra1.palosrv.com\MSSQLSERVER2022;Database=logic.systems.school.managment.panda;Trusted_Connection=false;MultipleActiveResultSets=true;Persist Security Info=true;User Id=AmeconAssociation;Password=5hbXx5%02;";
     }
 }
  
